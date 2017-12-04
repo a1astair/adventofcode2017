a35c5e1a03bc17734089582cfb8658d21c264288
a1astair <a.beaumont11@gmail.com>
2017-12-04 15:19:18 -0800

    Adding day3 and day4 code, Day3 I struggled with so I ended up needing some hints and help from other python answers

---
+++ day3/day3.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+input = 325489
+coords = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
+
+def part1(goal):
+    x = y = dx = 0
+    dy = -1
+    step = 0
+
+    while True:
+        step += 1
+        if goal == step:
+            return abs(x) + abs(y)
+        if (x == y) or (x < 0 and x == -y) or (x > 0 and x == 1-y):
+            dx, dy = -dy, dx
+        x, y = x+dx, y+dy
+
+def part2(goal):
+    x = y = dx = 0
+    dy = -1
+    grid = {}
+
+    while True:
+        total = 0
+        for offset in coords:
+            ox, oy = offset
+            if (x+ox, y+oy) in grid:
+                total += grid[(x+ox, y+oy)]
+        if total > int(goal):
+            return total
+        if (x, y) == (0, 0):
+            grid[(0, 0)] = 1
+        else:
+            grid[(x, y)] = total
+        if (x == y) or (x < 0 and x == -y) or (x > 0 and x == 1-y):
+            dx, dy = -dy, dx
+        x, y = x+dx, y+dy
+
+if __name__ == "__main__":
+    print(part1(325489))
+    print(part2(325489))
---
+++ day4/day4.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+def get_data():
+    with open('input.txt', 'r') as f:
+        data = [s.strip().split('\t') for s in f.readlines()]
+    return [[int(j) for j in i] for i in data]
+
+def part_one():
+    count = 0
+    with open('input.txt', 'r') as f:
+        for line in f.readlines():
+            lineset = set()
+            for word in line.split():
+                if word not in lineset:
+                    # Doesn't exist in list so add it on
+                    lineset.add(word)
+                else:
+                    count -= 1
+                    break
+            count += 1
+    print count
+
+def part_two():
+    invalid = 0
+    lines = 0
+    with open('input.txt', 'r') as f:
+        for line in f.readlines():
+            linelist = []
+            for word in line.split():
+                linelist.append(''.join(sorted(word)))
+            linelist.sort()
+            if len(linelist) != len(set(linelist)):
+                invalid += 1
+            print linelist
+            lines += 1
+    print lines - invalid
+
+if __name__ == "__main__":
+    # part_one()
+    part_two()
